60b257a0d854771d6400e32b70ff2f69fc272214
ness727 <gnsdl727@naver.com>
2023-12-28 16:08:07 +0900

    Start project

---
+++ backend/src/test/java/datamindhub/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package datamindhub.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
