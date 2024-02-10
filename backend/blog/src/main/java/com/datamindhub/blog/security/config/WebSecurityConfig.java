81f42c83fcea922ce7d596dd0e6e71d8c3203065
ness727 <gnsdl727@naver.com>
2024-02-10 21:21:46 +0900

    정적 자원 필터 무시 코드 추가 및 config 코드 정리

--- backend/blog/src/main/java/com/datamindhub/blog/security/config/WebSecurityConfig.java
+++ backend/blog/src/main/java/com/datamindhub/blog/security/config/WebSecurityConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +22,12 @@ public class WebSecurityConfig {
     //private final CustomJwtFilter customJwtFilter;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomCorsConfig customCorsConfig;
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return web -> web.ignoring()
+                .requestMatchers("/img/**", "/css/**", "/js/**");  // 정적 자원은 필터 무시
+    }
 
     // 특정 Http 요청에 대한 보안 설정
     @Bean
@@ -39,6 +46,9 @@ public class WebSecurityConfig {
                 // httpBasic 비활성화
                 .httpBasic(AbstractHttpConfigurer::disable)
 
+                // formLogin 비활성화
+                .formLogin(FormLoginConfigurer::disable)
+
                 // 세션 비활성화
 //                .sessionManagement(session -> session
 //                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -49,8 +59,8 @@ public class WebSecurityConfig {
                         .userInfoEndpoint(endpointConfig -> endpointConfig
                                 .userService(customOAuth2UserService)
                         )
+                        //.loginPage("/login")  // 커스텀 로그인 페이지 설정
                         .defaultSuccessUrl("/", true)
-                        //.loginPage("/login")
                 )
 
                 // 어느 경로를 인증받지 않고 사용할 수 있는지 설정
